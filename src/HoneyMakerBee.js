6e1cb22f66bc5bab2b0fbb22eb0e2dbdceb16774
Kate Jefferson <barbedbella@gmail.com>
2015-02-09 16:23:53 -0600

    Completed Taxonomy.

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,20 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
+
 
